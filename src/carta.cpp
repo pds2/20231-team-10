6765c2dddff9f6d4bbe4957a7a899c8dc8d591e2
sarah-aline <119525652+sarah-aline@users.noreply.github.com>
2023-06-20 20:11:34 -0300

    Update carta.cpp

--- src/carta.cpp
+++ src/carta.cpp
@@ -1,1 +1,29 @@
+#ifndef CARTA_CPP
+#define CARTA_CPP
 
+#include "Carta.hpp"
+
+#include <iostream>
+
+Carta::Carta(int valor, std::string nome, std::string naipe, bool rosto_baixo){
+    setValor(valor);
+    _nome = nome;
+    _naipe = naipe;
+    setRosto_baixo(rosto_baixo);
+}
+
+Carta::~Carta(){}
+
+//GETTERS E SETTERS ---------------------------------------
+
+int Carta::getValor(){return _valor;}
+void Carta::setValor(int valor){_valor = valor;}
+
+std::string Carta::getNome(){return _nome;}
+
+std::string Carta::getNaipe(){return _naipe;}
+
+bool Carta::getRosto_baixo(){return _rosto_baixo;}
+void Carta::setRosto_baixo(bool rosto_baixo){_rosto_baixo = rosto_baixo;}
+
+#endif
