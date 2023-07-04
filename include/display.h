a4e6446ed3601f7f628853b501f9a7a721852599
sarah-aline <119525652+sarah-aline@users.noreply.github.com>
2023-07-04 18:42:27 +0000

    Update display.h

--- include/display.h
+++ include/display.h
@@ -13,22 +13,16 @@ using namespace std;
 #define RIGHT_UP    4096
 #define RIGHT_DOWN  8192
 #define RIGHT_CLICK 16384
-#define LEFT_CLICK 4
-#define LEFT_DOUBLECLICK 24
-
-#define RIGHT_UP    4096
-#define RIGHT_DOWN  8192
-#define RIGHT_CLICK 16384
 #define RIGHT_DOUBLECLICK 32768
 
 #define MIDDLE_ROLLUP    1048576
 #define MIDDLE_ROLLDOWN  256
 
-const char joker[]	  = {0xE2, 0x98, 0xBA, 0};
-const char clubs[]    = {0xE2, 0x99, 0xA3, 0};
-const char diamonds[] = {0xE2, 0x99, 0xA6, 0};
-const char hearts[]   = {0xE2, 0x99, 0xA5, 0};
-const char spades[]   = {0xE2, 0x99, 0xA0, 0};
+const char joker[]= {0xE2, 0x98, 0xBA, 0};
+const char clubs[]= {0xE2, 0x99, 0xA3, 0};
+const char diamonds[]= {0xE2, 0x99, 0xA6, 0};
+const char hearts[]= {0xE2, 0x99, 0xA5, 0};
+const char spades[]= {0xE2, 0x99, 0xA0, 0};
 
 class display {
 public:
