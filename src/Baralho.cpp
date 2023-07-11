8383e1ef5e94484f62d46ff5323182c17cfaf112
Sanny Moreira <78819896+Snycms@users.noreply.github.com>
2023-07-10 23:35:00 -0300

    Update Baralho.cpp

--- src/Baralho.cpp
+++ src/Baralho.cpp
@@ -2,57 +2,107 @@
 #include "../include/Baralho.h"
 using namespace std;
 using namespace Baralhos;
-
+/**
+ * @brief Construct a new Baralhos:: Carta:: Carta object
+ * 
+ */
 Baralhos::Carta::Carta() {
     this->naipe = "";
     this->valor = "";
     this->codigo = -1;
     this->indice = -1;
 }
-
+/**
+ * @brief Construct a new Baralhos:: Carta:: Carta object
+ * 
+ * @param naipe 
+ * @param valor 
+ * @param codigo 
+ * @param indice 
+ */
 Baralhos::Carta::Carta(string naipe, string valor, int codigo, int indice) {
     this->naipe = naipe;
     this->valor = valor;
     this->codigo = codigo;
     this->indice = indice;
 }
-
+/**
+ * @brief retorna o naipe da carta
+ * 
+ * @return string 
+ */
 string Baralhos::Carta::getNaipe() {
     return this->naipe;
 }
-
+/**
+ * @brief retorna o valor da carta
+ * 
+ * @return string 
+ */
 string Baralhos::Carta::getValor() {
     return this->valor;
 }
-
+/**
+ * @brief rtorna o codigo da carta
+ * 
+ * @return int 
+ */
 int Baralhos::Carta::getCodigo() {
     return this->codigo;
 }
-
+/**
+ * @brief retorna o indice da carta
+ * 
+ * @return int 
+ */
 int Baralhos::Carta::getIndice() {
     return this->indice;
 }
-
+/**
+ * @brief seta o naipe da carta
+ * 
+ * @param naipe 
+ */
 void Baralhos::Carta::setNaipe(string naipe) {
     this->naipe = naipe;
 }
-
+/**
+ * @brief seta o valor da carta
+ * 
+ * @param valor 
+ */
 void Baralhos::Carta::setValor(string valor) {
     this->valor = valor;
 }
-
+/**
+ * @brief seta o código da carta
+ * 
+ * @param codigo 
+ */
 void Baralhos::Carta::setCodigo(int codigo) {
     this->codigo = codigo;
 }
-
+/**
+ * @brief seta o índice da carta
+ * 
+ * @param valor 
+ */
 void Baralhos::Carta::setIndice(int valor) {
     this->indice = valor;
 }
-
+/**
+ * @brief retorna o valor e naipe da carta
+ * 
+ * @return string 
+ */
 string Baralhos::Carta::getCarta(){ 
     return this->valor + " de " + this->naipe;
 }
-
+/**
+ * @brief retorna o símbolo da carta
+ * 
+ * @return string 
+ */
 string Baralhos::Carta::getSimboloCarta(){ 
     string simbolo =  "";
     if (this->naipe == "Copas") {
@@ -66,30 +116,57 @@ string Baralhos::Carta::getSimboloCarta(){
     }
     return this->valor + " " + simbolo + " | " + this->valor + " de " + this->naipe;
 }
-
+/**
+ * @brief sobrecarrega o operador >
+ * 
+ * @param str 
+ * @return true 
+ * @return false 
+ */
 bool Baralhos::Carta::operator > (const Carta& str) const {
     return (this->codigo > str.codigo);
 
 };
-
+/**
+ * @brief sobrecarrega o operador ==
+ * 
+ * @param str 
+ * @return true 
+ * @return false 
+ */
 bool Baralhos::Carta::operator == (const Carta& str) const {
     return (this->codigo == str.codigo);
 
 };
-
+/**
+ * @brief sobrecarrega o operador <
+ * 
+ * @param str 
+ * @return true 
+ * @return false 
+ */
 bool Baralhos::Carta::operator < (const Carta& str) const {
     return (this->codigo < str.codigo);
 };
-
+/**
+ * @brief Construct a new Baralhos:: Baralho Total:: Baralho Total object
+ * 
+ */
 Baralhos::BaralhoTotal::BaralhoTotal() {
     this->iniciarBaralho();
 }
-
+/**
+ * @brief Destroy the Baralhos:: Baralho Total:: Baralho Total object
+ * 
+ */
 Baralhos::BaralhoTotal::~BaralhoTotal() {
     this->todas_cartas.clear();
     this->cartas_usadas = 0;
 }
-
+/**
+ * @brief seta baralho
+ * 
+ */
 void Baralhos::BaralhoTotal::iniciarBaralho() {
     this->todas_cartas = vector<Carta>();
     this->cartas_usadas = 0;
@@ -105,13 +182,20 @@ void Baralhos::BaralhoTotal::iniciarBaralho() {
     }
     reverse(this->todas_cartas.begin(), this->todas_cartas.end());
 }
-
+/**
+ * @brief embaralha as cartas
+ * 
+ */
 void Baralhos::BaralhoTotal::embaralhar() {
     random_device rd;
     mt19937 generator(rd());
     shuffle(this->todas_cartas.begin(), this->todas_cartas.end(), generator);
 }
-
+/**
+ * @brief retorna numero total de cartas
+ * 
+ * @return int 
+ */
 int Baralhos::BaralhoTotal::getNumeroCartas()
 {
     return this->numero_cartas;
@@ -144,13 +228,20 @@ try {
     }
     return cartas;
 }
-
+/**
+ * @brief printa o baralho
+ * 
+ */
 void Baralhos::BaralhoTotal::printBaralho() {
     for (int i = 0; i < this->todas_cartas.size(); i++) {
         cout << this->todas_cartas[i].getCarta() << endl;
     }
 }
-
+/**
+ * @brief retorna cartas não distribuidas
+ * 
+ * @return int 
+ */
 int Baralhos::BaralhoTotal::getCartasRestantes() {
     return this->numero_cartas - this->cartas_usadas; 
 }
